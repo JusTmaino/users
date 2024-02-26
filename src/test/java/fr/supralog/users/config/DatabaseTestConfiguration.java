f84a1b58a8d2e2280e314813a2850ff9b841c2d2
JusTmaino <amine.elleuch1@gmail.com>
2024-02-26 23:36:39 +0100

    Add UserRepositoryTest and DatabaseTestConfiguration

---
+++ src/test/java/fr/supralog/users/config/DatabaseTestConfiguration.java
@@ -0,0 +1,16 @@
+package fr.supralog.users.config;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+/**
+ * Configuration for database tests
+ * we can use @ComponentScan in case we have embedded jar that contains useful components and configurations
+ * we can use @EnableTransactionManagement to make transactions work in Spring, by default spring activate it because we are using spring data
+ */
+@Configuration
+@EnableJpaRepositories(basePackages = "fr.supralog.users")
+@EntityScan(basePackages = "fr.supralog.users.entities")
+public class DatabaseTestConfiguration {
+}
